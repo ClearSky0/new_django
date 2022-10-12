7786566b295c3693e3b235af3b0db76edb4fa78b
Gary Desktop <frublob46@gmail.com>
2022-10-12 14:11:57 +0100

    Added .gitignore and an option for All_Auth.

--- new_django.py
+++ new_django.py
@@ -1,6 +1,7 @@
 import os
 
-root = input("\n\nLocation for new Django Project? ")
+root = input("\n\nLocation for new Django Project?\n"
+    "(NB. This is the directory above the project directory, current '.' is default) ") or '.'
 try:
     os.chdir(root)
 except Exception as e:
@@ -28,11 +29,18 @@ except Exception as e:
 project_folder = os.getcwd()
 print(f"\nProject folder created at: {project_folder}")
 
-app = input(f"\n\nWhat is the name of the first app within '{project}'? ('core' will be created regardless, blank for none) ")
+app = input(f"\n\nWhat is the name of the first app within '{project}'?\n"
+    "('core' will be created regardless, blank for none) ")
 if app == '':
     app_command = '@echo Not creating an app'
 else:
     app_command = f'python manage.py startapp {app}'
+
+all_auth_required = input("\n\nDo you want All_Auth installing? (y/n) ")
+if all_auth_required.lower() == 'y':
+    install_aa = 'pip install django-allauth'
+else:
+    install_aa = ''
 
 command = 'python -m venv venv' # Create the virtual env
 try:
@@ -53,10 +61,13 @@ except Exception as e:
 commands = [
     'python -m pip install --upgrade pip', # Upgrade pip
     'pip install django', # Install Django
+    install_aa, # All_Auth if required
+    'copy ' + os.path.dirname(os.path.abspath(__file__)) + '\.gitignore .', # Copy .gitignore 
     'pip freeze > requirements.txt', # Freeze as is to create requirements file
-    'django-admin.exe startproject core .', # start the project with an app call core
+    'django-admin.exe startproject core .', # start the project with core, for settings
     app_command, # start the project with the frontend app of the dev's choice
     'python manage.py migrate', # start the database
+    'git init', # Initialise git
     f'code {project_folder}', # open vscode in the project folder
     # 'python manage.py runserver', # run the server
 ]
@@ -64,13 +75,14 @@ commands = [
 # Subsequent commands need to have the venv activated as each call to os.system is a new cmd shell
 
 for command in commands:
-    command = activate + ' & ' + command
-    try:
-        print('\n', '*' * 5, 'Running', '*' * 5, '\n\t', command, '\n\n')
-        os.system(command)
-        # process = subprocess.run(command, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-        # print(process.stdout)
+    if command:
+        command = activate + ' & ' + command
+        try:
+            print('\n', '*' * 5, 'Running', '*' * 5, '\n\t', command, '\n\n')
+            os.system(command)
+            # process = subprocess.run(command, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+            # print(process.stdout)
 
-    except Exception as e:
-        print(e)
-        exit()
+        except Exception as e:
+            print(e)
+            exit()
